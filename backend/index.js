5686b377c10581e63e8d7b831b00ad8dab50ecfa
shubhzVidhate <shubhamvidhate302@gmail.com>
2025-10-08 17:47:11 +0530

    Connected To DB

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('❌ Error connecting to MongoDB:', err.message);
+  });
+
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-})
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
+
